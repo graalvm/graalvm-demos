b3d8c7e8f1594e72c8d02336e68f890d4974c9db
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2019-05-31 13:58:54 +0200

    Fixes for 19.0 and scalac

--- scala-days-2018/scalac-native/scalac-image-macros.sh
+++ scala-days-2018/scalac-native/scalac-image-macros.sh
@@ -12,4 +12,4 @@ for filename in $SCALA_LIB/*.jar; do
     SCALA_LIB_CLASSPATH=$filename:$SCALA_LIB_CLASSPATH
 done
 
-$GRAALVM_HOME/bin/native-image -cp $SCALA_LIB_CLASSPATH:macros/:$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar -H:SubstitutionResources=substitutions.json,substitutions-2.12.json -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json,macros/macros-config.json scala.tools.nsc.Main -H:Name=scalac $@
+$GRAALVM_HOME/bin/native-image --no-fallback --initialize-at-build-time -cp $SCALA_LIB_CLASSPATH:macros/:$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar -H:SubstitutionResources=substitutions.json,substitutions-2.12.json -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json,macros/macros-config.json scala.tools.nsc.Main -H:Name=scalac $@
