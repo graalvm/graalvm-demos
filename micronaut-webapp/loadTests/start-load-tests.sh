edd87c9d5c3062a198f6b2e5073667a418f01bd7
Esteban Ginez <esteban.ginez@oracle.com>
2019-09-04 17:13:51 -0700

    Fixes native-image generation support for ssl/https, documentation fixes for live routes, enables write request load tests

--- micronaut-webapp/loadTests/start-load-tests.sh
+++ micronaut-webapp/loadTests/start-load-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+OUT_DIR=${LOADTESTS_RESULTS:-out/}
 
-rm -rf out/
-jmeter -n -t LoadTest.jmx -J apiHostService=$TODOSERVICE_HOST -J apiHostPort=$TODOSERVICE_PORT -l loadTestResults.jtl
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
+jmeter -n -t LoadTest.jmx -J apiHostService=$TODOSERVICE_HOST -J apiHostPort=$TODOSERVICE_PORT -l $OUT_DIR/loadTestResults.jtl
 if [ $? -ne 0 ]; then
   echo "Error the jmeter command did not succeed"
   exit -1
@@ -9,12 +11,13 @@ fi
 
 echo "Load test finished. Creating results"
 
-jmeter -g loadTestResults.jtl -o out/
+jmeter -g $OUT_DIR/loadTestResults.jtl -o $OUT_DIR/report
 if [ $? -ne 0 ]; then
   echo "Error creating the report for results. Try re-running the test"
   exit -1
 fi
 
-echo "Results generated in out/index.html. Open the file to see the results(Ctrl+C to end test if running thru docker compose)"
+
+echo "Results generated in $OUT_DIR/report/index.html. Open the file to see the results(Ctrl+C to end test if running thru docker compose)"
 
 
