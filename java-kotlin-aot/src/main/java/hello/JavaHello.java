a23484cdfc3201a8a93ae0a64450c6c6f263f47f
Anthony Accioly <a.accioly@7rtc.com>
2020-05-10 19:54:07 +0100

    Print Java Version

--- java-kotlin-aot/src/main/java/hello/JavaHello.java
+++ java-kotlin-aot/src/main/java/hello/JavaHello.java
@@ -3,11 +3,20 @@ package hello;
 public class JavaHello {
   public static String JavaHelloString = "Hello from Java!";
 
+  public static String getJavaVersion() {
+    return String.format("Running on: %s %s %s (%s)",
+            System.getProperty("java.vm.name", ""),
+            System.getProperty("java.vendor", ""),
+            System.getProperty("java.vm.version", ""),
+            System.getProperty("java.runtime.version", System.getProperty("java.version", "")));
+  }
+
   public static String getHelloStringFromKotlin() {
     return KotlinHelloKt.getKotlinHelloString();
   }
 
   public static void main(String[] args) {
+    System.out.println(getJavaVersion());
     System.out.println(getHelloStringFromKotlin());
     System.out.println(KotlinHelloKt.getHelloStringFromJava());
   }
