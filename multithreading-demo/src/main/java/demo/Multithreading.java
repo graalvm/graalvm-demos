684ffb82559cff62262891fb4a9b3d979f15938f
Olya Gupalo <olga.gupalo@oracle.com>
2021-01-29 14:58:06 +0200

    Source code improvements

--- multithreading-demo/src/main/java/demo/Multithreading.java
+++ multithreading-demo/src/main/java/demo/Multithreading.java
@@ -1,12 +1,40 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Multithreading {
-    public final int count;
+    static int threadsStatic;
+    static int loadStatic;
+    static {
+        try {
+            String json = readInputStream(ClassLoader.getSystemResourceAsStream("config.json"));
+            ObjectMapper omap = new ObjectMapper();
+            JsonNode root = omap.readTree(json);
+            threadsStatic = root.path("config.threads").asInt();
+            loadStatic = root.path("config.load").asInt();
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(Multithreading.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-    public static void main(String[] args) throws InterruptedException, NumberFormatException {
-        Multithreading inst = new Multithreading(args.length == 0 ? 4 : Integer.parseInt(args[0]));
+    public final int count;
+    public final int load;
+
+    public static void main(String[] args) throws InterruptedException, NumberFormatException, JsonProcessingException {
+        Multithreading inst = new Multithreading(
+            args.length == 0 ? threadsStatic : Integer.parseInt(args[0]),
+            args.length > 1 ? Integer.parseInt(args[1]) : loadStatic            
+        );
 
         System.out.println("Synchronous execution for " + inst.count + " times.");
         long startTime = System.currentTimeMillis();
@@ -20,11 +48,12 @@ public class Multithreading {
         startTime = System.currentTimeMillis();
         inst.executeAsync();
         endTime = System.currentTimeMillis();
-        System.out.println("The execution of " + inst.count + " Threads takes: " + (endTime - startTime) + "ms");
+        System.out.println("The execution of " + inst.count + " Threads takes: " + (endTime - startTime) + "ms.");
     }
 
-    Multithreading(int count) {
+    Multithreading(int count, int load) {
         this.count = count;
+        this.load = load;
     }
 
     void executeSync() {
@@ -66,9 +95,24 @@ public class Multithreading {
 
     void execution() {
         Random rnd = new Random();
-        int[] ints = new int[10000000];
+        int[] ints = new int[this.load];
         for (int i = 0; i < ints.length; i++) {
             ints[i] = rnd.nextInt(100);
         }
+    }
+
+    private static String readInputStream(InputStream is) {
+        StringBuilder out = new StringBuilder();
+        try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+
+        } catch (IOException e) {
+            Logger.getLogger(Multithreading.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return out.toString();
     }
 }
