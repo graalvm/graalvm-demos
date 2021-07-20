e296f79a174e445138a3c6ac77ed2ceba1ffe33b
Aleksei Semin <alllexsm@gmail.com>
2021-07-20 07:25:29 +0200

    Use idiomatic Kotlin in the demo

--- java-kotlin-aot/src/main/kotlin/hello/KotlinHello.kt
+++ java-kotlin-aot/src/main/kotlin/hello/KotlinHello.kt
@@ -3,5 +3,5 @@ package hello
 val KotlinHelloString : String = "Hello from Kotlin!"
 
 fun getHelloStringFromJava() : String {
-    return JavaHello.JavaHelloString!!;
+    return JavaHello.JavaHelloString
 }
