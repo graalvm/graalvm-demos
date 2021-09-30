ca75d25d84c90da3a4f3ec66a619cf10e7ac2dd5
Mani Sarkar <sadhak001@gmail.com>
2021-09-30 14:30:04 +0100

    Comments: added minor comments to help with finding tags for GraalVM docker images

--- runDockerImage.sh
+++ runDockerImage.sh
@@ -7,7 +7,7 @@ set -o pipefail
 BASE_GRAALVM_VERSION="21.2.0"
 GRAALVM_JDK_VERSION="java11"
 GRAALVM_TYPE="all"   # other types are "native" or "polyglot"
-
+# See https://hub.docker.com/r/findepi/graalvm/tags, for details on version tag names
 DEFAULT_GRAALVM_VERSION="${BASE_GRAALVM_VERSION}-${GRAALVM_JDK_VERSION}-${GRAALVM_TYPE}"
 
 FULL_GRAALVM_VERSION="${FULL_GRAALVM_VERSION:-"${DEFAULT_GRAALVM_VERSION}"}"
@@ -16,8 +16,9 @@ GRAALVM_HOME_FOLDER="/graalvm"
 WORKDIR="/graalvm-demos"
 SHARED_FOLDER="${PWD}/shared"
 
-    ## By doing this we are extracting away the .m2 and .gradle folders from inside the container to the outside,
-## giving us a few advantages: speed gains, reduced image size, among others
+# By doing this we are extracting away the .m2 and .gradle folders from inside the
+# container to the outside, giving us a few advantages: speed gains, reduced image size,
+# among others
 M2_INSIDE_CONTAINER="/root/.m2/"
 M2_ON_HOST="${SHARED_FOLDER}/_dot_m2_folder"
 echo; echo "-- Creating/using folder '${M2_ON_HOST}' mounted as '.m2' folder inside the container (${M2_INSIDE_CONTAINER})"
@@ -25,6 +26,7 @@ mkdir -p ${M2_ON_HOST}
 
 GRADLE_REPO_INSIDE_CONTAINER="/root/.gradle/"
 GRADLE_REPO_ON_HOST="${SHARED_FOLDER}/_dot_gradle_folder"
+echo; echo "-- Creating/using folder '${GRADLE_REPO_ON_HOST}' mounted as '.gradle' folder inside the container (${GRADLE_REPO_INSIDE_CONTAINER})"
 mkdir -p ${GRADLE_REPO_ON_HOST}
 
 echo; echo "--- Running docker image for GraalVM version ${FULL_GRAALVM_VERSION} for ${WORKDIR}"; echo
