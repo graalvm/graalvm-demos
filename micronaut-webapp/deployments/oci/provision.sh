75fe77183a12c33cee039c0257a2a60c69a8b77b
Olga Gupalo <olga.gupalo@oracle.com>
2021-10-01 15:31:46 +0300

    Remove the demo as per agreement

--- micronaut-webapp/deployments/oci/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Open ports for the services to communicate
-sudo firewall-cmd --zone=public --permanent --add-port=8080-8085/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=8443/tcp
-sudo firewall-cmd --reload
-
-
-
