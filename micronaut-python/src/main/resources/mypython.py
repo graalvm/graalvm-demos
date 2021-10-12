4c1aff036bd74650cde73a6406deb395762c84ce
Olga Gupalo <olga.gupalo@oracle.com>
2021-10-12 23:49:31 +0300

    Remove micronaut-python demo by agreement

--- micronaut-python/src/main/resources/mypython.py
+++
@@ -1,8 +0,0 @@
-import pygal
-
-def bar_chart(numbers, label='Fibonacci'):
-  bar_chart = pygal.Bar(height=400)
-  bar_chart.add(label, numbers)
-  return bar_chart.render(is_unicode=True)
-
-bar_chart
