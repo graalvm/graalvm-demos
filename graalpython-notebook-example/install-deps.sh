ea0a6c8b638c0cbd18eaf360c655545e9e07dc53
Mani Sarkar <sadhak001@gmail.com>
2021-10-18 21:46:43 +0100

    graalpython demo: update the install-deps shellscript to use /home/satyasai/git-repos/OpenJDK/graal-stuff/graalvm-demos/docker-images for the venv path

--- graalpython-notebook-example/install-deps.sh
+++ graalpython-notebook-example/install-deps.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 set -o pipefail
 
+graalpython -Im ensurepip --upgrade --default-pip
 graalpython -m venv venv
-VIRTUAL_ENV=venv
+VIRTUAL_ENV=${PWD}/venv
 venv/bin/graalpython -m ginstall install matplotlib,Pillow,pandas
