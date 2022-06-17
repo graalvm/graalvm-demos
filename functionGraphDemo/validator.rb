a2c61cad395038c5c27872e5f4ba0393ef51f15b
Benoit Daloze <benoit.daloze@oracle.com>
2022-06-17 17:27:03 +0200

    [GR-38544] Workaround to handle TruffleString passed from JS
    * This can be removed once GR-38544 is fixed.

--- functionGraphDemo/validator.rb
+++ functionGraphDemo/validator.rb
@@ -2,6 +2,7 @@ module FunctionValidator
   ALLOWED_FUNCTIONS = %w[sin cos log sqrt]
 
   def self.validate(expr)
+    expr = expr.to_s # Workaround until GR-38544 is fixed
     expr.split(/x|[-+*\/()^]/).each do |term|
       unless term.empty?
         is_number = (Float(term) rescue false)
