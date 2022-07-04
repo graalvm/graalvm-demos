03271c737530e91972b732088040da068de0d5e4
Olya Gupalo <olga.gupalo@oracle.com>
2022-07-04 16:39:24 +0200

    Include build scripts for fortune-demo

---
+++ fortune-demo/staticfortune/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+mvn clean compile
+# Running the agent
+mvn -Pnative -Dagent exec:exec@java-agent
+# Building the native executable
+mvn -Pnative -Dagent package
+# Running the application with Maven and as a native executable
+mvn -Pnative exec:exec@native
+./target/staticfortune
