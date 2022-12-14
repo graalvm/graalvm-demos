f9bad7ec1e9814cb1dc9258578b1576bcf9c05e6
Lesia Chaban <lesia.chaban@oracle.com>
2022-12-14 14:38:48 +0200

    Add Build a Native Executable with Reflection example to GraalVM Demos

---
+++ build-with-reflection-example/ReflectionExample.java
@@ -0,0 +1,34 @@
+
+import java.lang.reflect.Method;
+
+class StringReverser {
+
+    static String reverse(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+}
+
+class StringCapitalizer {
+
+    static String capitalize(String input) {
+        return input.toUpperCase();
+    }
+}
+
+public class ReflectionExample {
+
+    public static void main(String[] args) throws ReflectiveOperationException {
+        if (args.length == 0) {
+            System.err.println("You must provide the name of a class, the name of its method and input for the method");
+            return;
+        }
+        String className = args[0];
+        String methodName = args[1];
+        String input = args[2];
+
+        Class<?> clazz = Class.forName(className);
+        Method method = clazz.getDeclaredMethod(methodName, String.class);
+        Object result = method.invoke(null, input);
+        System.out.println(result);
+    }
+}
