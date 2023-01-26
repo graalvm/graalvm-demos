b9f54efba951635995e3374de72c6e132c2e8acb
Lesia Chaban <lesia.chaban@oracle.com>
2023-01-26 16:20:07 +0200

    Change in copyright header

--- graalpy-notebook-example/src/main/java/com/oracle/example/javapython/Main.java
+++ graalpy-notebook-example/src/main/java/com/oracle/example/javapython/Main.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Software Architecture Group, Hasso Plattner Institute
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -38,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+ */
 import java.awt.Button;
 import java.awt.Canvas;
 import java.awt.Component;
