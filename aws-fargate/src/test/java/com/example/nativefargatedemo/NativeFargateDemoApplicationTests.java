598853f9a8e3e20ac98f2a9a16ba6efb69ebfe75
Eric Gadbois <eric.gadbois@oracle.com>
2023-06-01 13:48:28 -0700

    added fargate demo

---
+++ aws-fargate/src/test/java/com/example/nativefargatedemo/NativeFargateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativefargatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeFargateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
