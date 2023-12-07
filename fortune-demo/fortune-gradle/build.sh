a2910a997c0d025b3b914662c0ef0f7ba924f01c
Olya Gupalo <olga.gupalo@oracle.com>
2023-12-07 13:53:26 +0100

    Run only agent check

--- fortune-demo/fortune-gradle/build.sh
+++ fortune-demo/fortune-gradle/build.sh
@@ -7,10 +7,8 @@ set -e
 ./gradlew -Pagent run
 # Copy metadata into /META-INF/native-image directory
 ./gradlew metadataCopy --task run --dir src/main/resources/META-INF/native-image
-# Build a native executable using configuration
-./gradlew nativeCompile
-# Run the native executable
-./fortune/build/native/nativeCompile/fortune
+# Build a native executable using configuration (`nativeRun` compiles the app, then invokes `nativeCompile`, and then runs the native binary)
+./gradlew nativeRun
 # Run JUnit tests
 ./gradlew nativeTest
 # Run tests on JVM with the agent
