99615953c86d3e9740475dddd58839c92607b421
Olya Gupalo <olga.gupalo@oracle.com>
2023-12-07 16:45:33 +0100

    Synchronize Jibber demo with other variants of this application

--- spring-native-image/src/main/java/com/example/benchmarks/jibber/Jabberwocky.java
+++ spring-native-image/src/main/java/com/example/benchmarks/jibber/Jabberwocky.java
@@ -7,13 +7,9 @@ package com.example.benchmarks.jibber;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
 import rita.RiMarkov;
 
-/**
- * Utility class, that is a Singleton, that generates nonsense verse in the style of
- * the poem Jabberocky, by Lewis Carol. It does this using a Markov Chain to model
- * the text of the original poem.
- */
 @Service
 @Scope("singleton")
 public class Jabberwocky {
@@ -27,7 +23,7 @@ public class Jabberwocky {
     private void loadModel() {
         //
         String text = "’Twas brillig, and the slithy toves " +
-                "Did gyre and gimble in the wabe:" +
+                "Did gyre and gimble in the wabe: " +
                 "All mimsy were the borogoves, " +
                 "And the mome raths outgrabe. " +
                 "Beware the Jabberwock, my son! " +
@@ -48,7 +44,7 @@ public class Jabberwocky {
                 "He went galumphing back. " +
                 "And hast thou slain the Jabberwock? " +
                 "Come to my arms, my beamish boy! " +
-                "O frabjous day! Callooh! Callay!” " +
+                "O frabjous day! Callooh! Callay! " +
                 "He chortled in his joy. " +
                 "’Twas brillig, and the slithy toves " +
                 "Did gyre and gimble in the wabe: " +
@@ -57,18 +53,15 @@ public class Jabberwocky {
         this.r = new RiMarkov(3);
         this.r.addText(text);
     }
-
     public String generate() {
-        return generate(10);
-    }
-
-    public String generate(final int numLines) {
-        String[] lines = this.r.generate(numLines);
-        StringBuffer b = new StringBuffer();
-        for (int i=0; i< lines.length; i++) {
-            b.append(lines[i]);
+        String[] lines = this.r.generate(10);
+        StringBuilder b = new StringBuilder();
+        //
+        for (String line : lines ) {
+            b.append(line);
             b.append("<br/>\n");
         }
+        //
         return b.toString();
     }
 
