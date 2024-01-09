4fdd683a89b96067745d398286dbc40c4fb07646
Shaun Smith <shaun.smith@oracle.com>
2024-01-09 16:20:28 -0500

    Enabled experimental options for mostly static linking.

--- tiny-java-containers/jwebserver/build-mostly.sh
+++ tiny-java-containers/jwebserver/build-mostly.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-native-image -Ob -H:+StaticExecutableWithDynamicLibC -m jdk.httpserver -o jwebserver.mostly
+native-image -Ob -H:+UnlockExperimentalVMOptions -H:+StaticExecutableWithDynamicLibC -m jdk.httpserver -o jwebserver.mostly
 
 # Distroless Base (provides glibc)
 docker build . -f Dockerfile.distroless-base.mostly -t jwebserver:distroless-base.mostly
