d2542901c2b9b1545bb22617ea9511bb1777cb98
Olya Gupalo <olga.gupalo@oracle.com>
2024-06-28 16:57:36 +0300

    Revert back to use regex instead of globs to specify path to a resource file

--- fortune-demo/fortune-gradle/build.sh
+++ fortune-demo/fortune-gradle/build.sh
@@ -7,3 +7,5 @@ set -e
 ./gradlew -Pagent run
 # Build a native executable using configuration (`nativeRun` compiles the app, then invokes `nativeCompile`, and then runs the native binary)
 ./gradlew nativeCompile
+# Run the application
+./fortune/build/native/nativeCompile/fortune
--- fortune-demo/fortune-maven/build.sh
+++ fortune-demo/fortune-maven/build.sh
@@ -5,5 +5,5 @@ set -e
 
 # Build the native executable
 mvn -Pnative package
-# Run the application with Maven and from a native executable
+# Run the application
 ./target/fortune
