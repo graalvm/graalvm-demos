eb6624db54bd3bd1cbea979dbefc2264bfe8b7f3
Fabio Niephaus <fabio.niephaus@oracle.com>
2024-09-16 17:34:01 +0200

    Add GraalWasm demos using Photon.

---
+++ graalwasm/photon-micronaut/src/main/java/com/example/DemoController.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://opensource.org/license/UPL.
+ */
+
+package com.example;
+
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller
+public class DemoController {
+    private final PhotonService photonService;
+
+    public DemoController(PhotonService photonService) {
+        this.photonService = photonService;
+    }
+
+    @Get("/photo/{effectName}")
+    @Produces(MediaType.IMAGE_PNG)
+    public byte[] renderPhoto(@Parameter String effectName) {
+        return photonService.processImage(effectName);
+    }
+}
