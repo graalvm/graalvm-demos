a456ad6f0926aa093ed7e86d2af71129aa61aef8
Olya Gupalo <olga.gupalo@oracle.com>
2024-12-05 18:11:03 +0200

    Enable the check for --enable-sbom=class-level

--- native-image/embed-sbom/run.sh
+++ native-image/embed-sbom/run.sh
@@ -5,6 +5,6 @@ native-image -Ob --enable-sbom=cyclonedx -m jdk.httpserver -o jwebserver
 
 # Requires GraalVM for JDK 24 Early Access build 24.ea.23-graal or later:
 # sdk install java 24.ea.23-graal
-# cd sbom-test
-# mvn clean package
-# mvn -Pnative package
+cd sbom-test
+mvn clean package
+mvn -Pnative package
