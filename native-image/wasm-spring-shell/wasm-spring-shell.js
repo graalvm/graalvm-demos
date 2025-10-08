fc1e5273a7e301de6bf19cac269e8aaa41c8d89f
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-10-08 09:26:43 +0200

    Add UI for wasm-spring-shell.

--- native-image/wasm-spring-shell/wasm-spring-shell.js
+++ native-image/wasm-spring-shell/wasm-spring-shell.js
@@ -287,6 +287,48 @@ class ConsoleWriter {
 var stdoutWriter = new ConsoleWriter(console.log);
 var stderrWriter = new ConsoleWriter(console.error);
 
+class OutputWriter {
+    constructor(domElement) {
+        this.line = "";
+        this.newline = "\n".charCodeAt(0);
+        this.closed = false;
+        this.domElement = domElement;
+    }
+
+    printChars(chars) {
+        let index = chars.lastIndexOf(this.newline);
+
+        if (index >= 0) {
+            this.line += charArrayToString(chars.slice(0, index));
+            this.writeLine();
+            chars = chars.slice(index + 1);
+        }
+
+        this.line += charArrayToString(chars);
+    }
+
+    writeLine() {
+        window.appendOutput(this.line);
+        this.line = "";
+    }
+
+    flush() {
+        // nothing to do
+    }
+
+    close() {
+        if (this.closed) {
+            return;
+        }
+        this.closed = true;
+
+        this.flush();
+    }
+}
+
+var stdoutWriter = new OutputWriter(document.getElementById("output"));
+
+
 /*
  * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -2777,47 +2819,47 @@ data.wasm = await wasmInstantiate(config, vmArgs);
 
 })();
 
-(function() {
-/*
- * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
+// (function() {
+// /*
+//  * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
+//  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//  *
+//  * This code is free software; you can redistribute it and/or modify it
+//  * under the terms of the GNU General Public License version 2 only, as
+//  * published by the Free Software Foundation.  Oracle designates this
+//  * particular file as subject to the "Classpath" exception as provided
+//  * by Oracle in the LICENSE file that accompanied this code.
+//  *
+//  * This code is distributed in the hope that it will be useful, but WITHOUT
+//  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+//  * version 2 for more details (a copy is included in the LICENSE file that
+//  * accompanied this code).
+//  *
+//  * You should have received a copy of the GNU General Public License version
+//  * 2 along with this work; if not, write to the Free Software Foundation,
+//  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+//  *
+//  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+//  * or visit www.oracle.com if you need additional information or have any
+//  * questions.
+//  */
 
-/**
- * Try to load commandline arguments for various JS runtimes.
- */
-function load_cmd_args() {
-    if (typeof process === "object" && "argv" in process) {
-        // nodejs
-        return process.argv.slice(2);
-    } else if (typeof scriptArgs == "object") {
-        // spidermonkey
-        return scriptArgs;
-    }
+// /**
+//  * Try to load commandline arguments for various JS runtimes.
+//  */
+// function load_cmd_args() {
+//     if (typeof process === "object" && "argv" in process) {
+//         // nodejs
+//         return process.argv.slice(2);
+//     } else if (typeof scriptArgs == "object") {
+//         // spidermonkey
+//         return scriptArgs;
+//     }
 
-    return ['help'];
-}
+//     return ['help'];
+// }
 
-const config = new GraalVM.Config();
-GraalVM.run(load_cmd_args(),config).catch(console.error);
-})();
+// const config = new GraalVM.Config();
+// GraalVM.run(load_cmd_args(),config).catch(console.error);
+// })();
