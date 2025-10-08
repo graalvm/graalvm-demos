9e8e3968107567ce5b54fb4b48f4b3f45c340ac4
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-10-08 09:36:55 +0200

    Implement wams-spring-shell.

---
+++ native-image/wasm-spring-shell/src/main/java/com/example/wasm_spring_shell/MyCommands.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://opensource.org/license/UPL.
+ */
+
+package com.example.wasm_spring_shell;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class MyCommands {
+
+    @ShellMethod(key = "hello")
+    public String hello(@ShellOption(defaultValue = "Spring") String arg) {
+        return "Hello " + arg + " from WebAssembly built with GraalVM!";
+    }
+    
+}
