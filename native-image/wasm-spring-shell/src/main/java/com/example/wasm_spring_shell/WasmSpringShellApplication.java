1ae9a5de792cfb201b98543b34b3a386dc8efd7d
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-10-08 09:30:54 +0200

    Add fresh Spring Shell template.
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.6&packaging=jar&jvmVersion=25&groupId=com.example&artifactId=wasm-spring-shell&name=wasm-spring-shell&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.wasm-spring-shell&dependencies=native,spring-shell

---
+++ native-image/wasm-spring-shell/src/main/java/com/example/wasm_spring_shell/WasmSpringShellApplication.java
@@ -0,0 +1,13 @@
+package com.example.wasm_spring_shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasmSpringShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasmSpringShellApplication.class, args);
+	}
+
+}
