68a312514c2c96d70aab05258db1ec7696e4a7b4
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-10-08 09:30:54 +0200

    Add fresh Spring Shell template.
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.6&packaging=jar&jvmVersion=25&groupId=com.example&artifactId=wasm-spring-shell&name=wasm-spring-shell&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.wasm-spring-shell&dependencies=native,spring-shell

---
+++ native-image/wasm-spring-shell/src/test/java/com/example/wasm_spring_shell/WasmSpringShellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wasm_spring_shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasmSpringShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
