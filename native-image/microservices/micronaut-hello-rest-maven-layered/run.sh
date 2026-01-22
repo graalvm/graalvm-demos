2884a7927748e9bcfb02b5f63f205f6b2774d8a0
Sacha Coppey <sacha.coppey@oracle.com>
2026-01-22 19:40:15 +0100

    Add a Native Image Layers demo for a simple Micronaut application with Micronaut in the application layer.

--- native-image/microservices/micronaut-hello-rest-maven-layered/run.sh
+++ native-image/microservices/micronaut-hello-rest-maven-layered/run.sh
@@ -4,5 +4,15 @@ set -ex
 ./mvnw --no-transfer-progress clean install -Dpackaging=native-image -Pbase-layer
 ./mvnw --no-transfer-progress clean package -Dpackaging=native-image -Papp-layer
 ./app-layer-target/layered-app &
+pid=$!
+sleep 5
+curl "http://localhost:8080/hello"
+kill $pid
+
+cd base-layer
+../mvnw --no-transfer-progress clean install -Dpackaging=native-image
+cd ..
+./mvnw --no-transfer-progress clean package -Dpackaging=native-image -Pmicronaut-app-layer
+./app-layer-target/layered-app &
 sleep 5
 curl "http://localhost:8080/hello"
